5ac1e435f1476708cd3ca2aa2d1f55b00cfb6779
davefelcey <davefelcey@gmail.com>
2017-07-17 21:06:31 +0100

    Update app.js
    Changed logging

--- src/app.js
+++ src/app.js
@@ -343,7 +343,7 @@ class FacebookBot {
                          }
                          console.log('Response data: ' + response.result.first_name);
                     }
-                    console.log('Result: ' + response.first_name);
+                    console.log('Result: ' + response);
 
                 });
 
